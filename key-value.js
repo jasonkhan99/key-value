af839a2c326674d6792013e1ddebd1c0f5d22b6d
Jason Khan <jkhan@ascendsoftware.com>
2021-10-15 13:37:20 -0700

    add invalid syntax message

--- key-value.js
+++ key-value.js
@@ -10,14 +10,26 @@ const rl = readline.createInterface({
 
 function main() { rl.question("> ", function(input) {
   let inputFilter = input.split(" ").filter(c => c).join(" ");
-  if (inputFilter.substring(0, 4).toLowerCase() === "put ") {
+  if (inputFilter.substring(0, 3).toLowerCase() === "put") {
+    if (inputFilter.split(" ").length !== 3) {
+      console.log("Invalid syntax");
+    } else {
     func.storage.put(inputFilter.split(" ")[1], inputFilter.split(" ")[2]);
     console.log("ok");
-  } else if (inputFilter.substring(0, 6).toLowerCase() === "fetch ") {
-    console.log(func.storage[inputFilter.split(" ")[1]]);
-  } else if (inputFilter.substring(0, 5).toLowerCase() === "exit ") {
-    console.log("Bye!");
-    process.exit();
+    }
+  } else if (inputFilter.substring(0, 5).toLowerCase() === "fetch") {
+    if (inputFilter.split(" ").length !== 2) {
+      console.log("Invalid syntax");
+    } else { 
+      console.log(func.storage[inputFilter.split(" ")[1]]);
+    }
+  } else if (inputFilter.substring(0, 4).toLowerCase() === "exit") {
+    if (inputFilter.length !== 4) {
+      console.log("Invalid syntax");
+    } else { 
+      console.log("Bye!");
+      process.exit();
+    }
   } else {
     console.log("Unknown command. Known commands are: put, fetch, exit");
   }
